ee0d1c17779fe7598cb47580b2357d3e689e4acd
RohanA <rohanagarwal0192@gmail.com>
2024-05-06 23:24:06 +1000

    Dashboard add chat box and section for displaying chat

--- frontend/src/Dashboard/Dashboard.js
+++ frontend/src/Dashboard/Dashboard.js
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './dashboard.css'
+import { IoMdSend } from "react-icons/io";
 
 const Dashboard = ({ isSidebarOpen }) => {
+  const [sendEnabled, setSendEnabled] = useState(false);
+
+  function chatInputChange(e) {
+    console.log(e.target.value, e.target.value === null, e.target.value === '');
+    if (e.target.value !== null && e.target.value !== '') {
+      setSendEnabled(true);
+    } else {
+      setSendEnabled(false);
+    }
+  }
+
   return (
     <div className="flex-1 px-14 py-8 bg-app-bg">
       <div className='h-full px-2 py-10'>
-        <div className='border-white-100 border-2 h-full rounded-lg p-10'>
-          <h1>Dashboard</h1>
-          <p>Main content area.</p>
+        {/* Chat Area */}
+        <div className='flex flex-col border-white-100 border-2 h-full rounded-lg'>
+          <div className='p-10 flex-grow'>
+            <h1>Dashboard</h1>
+            <p>Main content area.</p>
+          </div>
+          {/* Chat input box */}
+          <div className='flex-shrink-0'>
+            <div className='relative flex flex-col justify-center items-center rounded-lg w-full'>
+              <div className="absolute top-0 h-2px bg-top-gradient-border w-full"></div>
+            </div>
+            <div className='flex flex-row rounded-br-lg rounded-bl-lg bg-app-bg border-t-white-100 border-t-2'>
+              <input onChange={(e) => chatInputChange(e)} className='outline-none bg-app-bg px-10 py-3 w-full rounded-br-lg rounded-bl-lg border-bl-2' type='text' placeholder='Enter your prompt here...' />
+              <button className={`mx-2 my-3 px-3 rounded-lg border-2 border-l-2 ${sendEnabled ? 'cursor-pointer border-white-100 text-white' : 'cursor-not-allowed disabled border-gray-600 text-slate-600'}`}>
+                <div className='flex flex-row justify-center items-center'>
+                  <IoMdSend />
+                  <p className='pl-1'>Send</p>
+                </div>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
       <div className="w-full bg-app-bg flex-row">
